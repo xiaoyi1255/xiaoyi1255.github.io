c4135c4a0df7d31ba3a19be6b0f66dca52d132dd
xiaoyi <jming.tai@backgardon.com>
2023-07-03 18:07:26 +0800

    genxin

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+yarn 
 # 构建 Vitepress 项目
 yarn build
 
@@ -9,22 +10,25 @@ yarn build
 # 将构建生成的静态文件复制到临时目录
 cp -r docs/.vitepress/dist/* temp_deploy
 
+git add .
+git commit -m "Auto"
+
 # 切换到 gh-pages 分支
 git checkout gh-pages
 
 # 删除旧的构建文件
-rm -rf *
+# rm -rf *
 
 # 将临时目录中的文件复制到当前目录
 cp -r temp_deploy/* .
 
 # 删除临时目录
-rm -rf temp_deploy
+# rm -rf temp_deploy
 
 # 提交更改并推送到远程仓库
-git add .
-git commit -m "Auto deploy"
-git push origin gh-pages
+# git add .
+# git commit -m "Auto deploy"
+# git push origin gh-pages
 
 # 切换回主分支
-git checkout master
+# git checkout master
