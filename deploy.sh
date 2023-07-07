0c73fcd61bb4c01bd194e83420fefaf70c2670c4
静ming <jming.tai@backgardon.com>
2023-07-07 10:33:27 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,13 @@ mkdir temp_deploy
 # 将构建生成的静态文件复制到临时目录
 cp -r docs/.vitepress/dist/* temp_deploy
 
+find . -mindepth 1 -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name 'temp_deploy' -exec rm -rf {} \;
+git add .
+git commit -m "deploy"
+
 git ch gh-pages
 
-find . -mindepth 1 -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name 'temp_deploy' -exec rm -rf {} \;
+find . -mindepth 1 -maxdepth 1 ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 
 git merge temp
 
