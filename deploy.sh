7f80dd1bc374d9321440a3997e7d3a4e797e7b8d
静ming <jming.tai@backgardon.com>
2023-07-07 10:40:05 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,9 @@ git ch gh-pages
 
 find . -mindepth 1 -maxdepth 1 ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 
+git add .
+git commit -m "deploy"
+
 git merge temp
 
 find . -mindepth 1 -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name 'temp_deploy' -exec rm -rf {} \;
