ac2b9a7883bdc65145fbb6e1c1efd69e2734e401
静ming <jming.tai@backgardon.com>
2023-12-25 19:29:47 +0800

    deploy

--- docs/guide/js/简单/709. 转换成小写字母.ts
+++
@@ -1,5 +0,0 @@
-function toLowerCase(s: string): string {
-  return s.toLowerCase();
-};
-
-console.log(toLowerCase("Hello World!"));
--- docs/guide/project/bt/linux.md
+++
@@ -1,2 +0,0 @@
-## 前言
-记录自己从window server 2012 重装为	CentOS 7.9 的踩坑之旅。
